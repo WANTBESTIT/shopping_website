8641e8d58cd3bb438e9794bb758bca79d4117c4b
WANTITBEST <1059630903@qq.com>
2020-08-22 11:10:16 +0800

    商品详情页

---
+++ shopping-page-ms/src/main/java/com/gec/shopping/AppPage.java
@@ -0,0 +1,17 @@
+package com.gec.shopping;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan("com.gec.shopping.org.gec.security.mapper")
+public class AppPage {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPage.class, args);
+	}
+}
